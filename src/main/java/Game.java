c883ea1bca6c9b5f39d69c93eff04e82c561c2ca
Catherine Lawrey <cmlawrey@gmail.com>
2015-07-07 23:35:49 +0100

    First commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -11,35 +11,35 @@ public class Game {
     public static void main(String[] args) {
         Person p = new Person(3, 3);
         Maze maze = new Maze(
-                "##########",
-                "#........#",
-                "#.#.#.#.##",
-                "#.#.#.####",
-                "#.#...#..#",
-                "#...#....X",
-                "##########"
+                "┏━━━━━━━━┓",
+                "┃......☕..┃",
+                "┃.┃.┃.┃.┃.┃",
+                "┃.┃.┃.┣━┫.┃",
+                "┃.┃...┃.┗━┫",
+                "┃...┃.....X",
+                "┗━━━┻━━━━━┛"
         );
         System.out.println("Welcome to the maze. ");
         while (maze.at(p.x, p.y) != 'X') {
             System.out.println();
 
             // This prints the map.
-            for (int j = -2; j <= 2; j++) {
+            for (int j = 2; j >= -2; j--) {
                 for (int i = -2; i <= 2; i++) {
                     if (i == 0 && j == 0)
                         System.out.print("☺");
                     else
-                        System.out.print(maze.at(p.x + i, p.y - j));
+                        System.out.print(maze.at(p.x + i, p.y + j));
                 }
                 System.out.println();
             }
 
             // This prints where you can go.
             List<String> dir = new ArrayList<>();
-            if (maze.at(p.x - 1, p.y) != '#') dir.add("(w)est");
-            if (maze.at(p.x + 1, p.y) != '#') dir.add("(e)ast");
-            if (maze.at(p.x, p.y + 1) != '#') dir.add("(n)north");
-            if (maze.at(p.x, p.y - 1) != '#') dir.add("(s)outh");
+            if (!maze.isWall(p.x - 1, p.y)) dir.add("(w)est");
+            if (!maze.isWall(p.x + 1, p.y)) dir.add("(e)ast");
+            if (!maze.isWall(p.x, p.y + 1)) dir.add("(n)north");
+            if (!maze.isWall(p.x, p.y - 1)) dir.add("(s)outh");
 
             System.out.println("you can go " + dir + " x: " + p.x + " y: " + p.y);
 
@@ -48,7 +48,7 @@ public class Game {
             switch (ans) {
                 case "w":
                 case "west":
-                    if (maze.at(p.x - 1, p.y) != '#')
+                    if (!maze.isWall(p.x - 1, p.y))
                         p.x--;
                     else
                         System.out.println("You can't go west");
@@ -56,7 +56,7 @@ public class Game {
 
                 case "e":
                 case "east":
-                    if (maze.at(p.x + 1, p.y) != '#')
+                    if (!maze.isWall(p.x + 1, p.y))
                         p.x++;
                     else
                         System.out.println("That's too scary !!!");
@@ -64,7 +64,7 @@ public class Game {
 
                 case "n":
                 case "north":
-                    if (maze.at(p.x, p.y + 1) != '#')
+                    if (!maze.isWall(p.x, p.y + 1))
                         p.y++;
                     else
                         System.out.println("no can do");
@@ -72,7 +72,7 @@ public class Game {
 
                 case "s":
                 case "south":
-                    if (maze.at(p.x, p.y - 1) != '#')
+                    if (!maze.isWall(p.x, p.y - 1))
                         p.y--;
                     else
                         System.out.println("Try again");
@@ -107,5 +107,9 @@ class Maze {
         if (y < 0 || y >= layout.length || x < 0 || x >= layout[layout.length - 1 - y].length())
             return '#';
         return layout[layout.length - 1 - y].charAt(x);
+    }
+
+    public boolean isWall(int x, int y) {
+        return (at(x, y) & 0x2580) == 0x2500;
     }
 }
